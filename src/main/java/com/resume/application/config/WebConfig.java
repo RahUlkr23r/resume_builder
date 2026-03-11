b9e2dcad2a5bf69ef27dba6d67f7ea99f691b917
RahUlkr23r <rahulkumar8684singh@gmail.com>
2026-03-11 17:02:42 +0530

    add docker file

--- src/main/java/com/resume/application/config/WebConfig.java
+++ src/main/java/com/resume/application/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("https://dreamy-cranachan-baac8b.netlify.app")
+            .allowedOrigins("resume-buildergec.netlify.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("*")
             .allowCredentials(true);
