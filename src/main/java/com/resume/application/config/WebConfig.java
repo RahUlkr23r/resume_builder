dc8c337292f089e1f3e2694601ea49448ff5fa1b
RahUlkr23r <rahulkumar8684singh@gmail.com>
2026-03-11 19:27:54 +0530

    add docker file

--- src/main/java/com/resume/application/config/WebConfig.java
+++ src/main/java/com/resume/application/config/WebConfig.java
@@ -1,23 +1,16 @@
 package com.resume.application.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins(
-                        "https://resume-frontend-cybercrime.netlify.app",
-                      "https://jolly-kheer-3cf01e.netlify.app/",
-                        "https://your-frontend.vercel.app",
-                        "http://localhost:5173",
-                        "http://localhost:3000"
-                )
-                .allowedMethods("*")
+        registry.addMapping("/**")
+                .allowedOrigins("https://jolly-kheer-3cf01e.netlify.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
